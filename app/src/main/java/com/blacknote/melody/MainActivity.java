d1e1cc3d687ca90c2d8f075dffaf8b7292db0c5c
BLACKNOTES <amirshaygan76@gmail.com>
2017-12-22 13:07:38 +0330

    Initial commit

---
+++ app/src/main/java/com/blacknote/melody/MainActivity.java
@@ -0,0 +1,16 @@
+package com.blacknote.melody;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static android.graphics.Color.WHITE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
